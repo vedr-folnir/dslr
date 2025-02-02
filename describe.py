2ef130cd40512ceea9d489821290105a2522893d
vedr-folnir <124672580+vedr-folnir@users.noreply.github.com>
2025-02-02 21:12:49 +0100

    new describe to accomodate other csv files

--- describe.py
+++ describe.py
@@ -40,41 +40,46 @@ if (len(sys.argv) > 1):
                 for j in range(0,size):
                     if (elem[j]):
                         tab[j].append(elem[j])
-            print("\t\t",end="")
+            print("\t    ",end="")
             for i in range(6,19):
-                print(f"{name[i]:>9}",end="\t")
-            print("\ncount", end="\t\t")
+                print(f"{name[i]:>12}",end="\t")
+            print("\ncount", end="")
             for t in tab:
                 t.sort()
-                print(f"{len(t):>7}", end="\t\t")  
-            print("\nmean", end="\t\t")
+                print(f"{len(t):>12}", end="\t ")  
+            print("\nmean", end="\t")
             for t in tab:
                 mean=0
                 for val in t:
                     mean+=val
                 mean = round(mean/len(tab[0]),6)
                 std.append(round(sqrt(abs(mean)),6))
-                print(f"{mean:>10}", end="\t")
+                print(f"{mean:>12}", end="\t")
             print("\nstd", end="\t\t")
             for i in range(0,len(tab)):
-                print(f"{std[i]:>10}",end="\t")
+                print(f"{std[i]:>12}",end="\t")
             print("\nmin", end="\t\t")
             for t in tab:
-                print(f"{round(t[0],6):>10}",end="\t")
+                print(f"{round(t[0],6):>12}",end="\t")
             print("\n25%", end="\t\t")
             for t in tab:
-                print(f"{round(t[round(len(t)/4)],6):>10}",end="\t")
+                print(f"{round(t[round(len(t)/4)],6):>12}",end="\t")
             print("\n50%", end="\t\t")
             for t in tab:
-                print(f"{round(t[round(len(t)/2)],6):>10}",end="\t")
+                print(f"{round(t[round(len(t)/2)],6):>12}",end="\t")
             print("\n75%", end="\t\t")
             for t in tab:
-                print(f"{round(t[round(len(t)/3)*2],6):>10}",end="\t")
+                print(f"{round(t[round(len(t)/3)*2],6):>12}",end="\t")
             print("\nmax", end="\t\t")
             for t in tab:
-                print(f"{round(t[-1],6):>10}",end="\t")
+                print(f"{round(t[-1],6):>12}",end="\t")
             
     except:
         print("Fatal error while opening csv")
 else:
     print("Fatal error")
+
+tab[-1].sort()
+tab[-2].sort()
+print ("\n", tab[-1][0])
+print (tab[-2][0])
---
+++ new_describe.py
@@ -0,0 +1,79 @@
+import csv
+from math import sqrt
+import sys
+import pandas
+
+def is_number(s):
+    # print(s, s.replace(".", "", 1).isdigit() and s.count(".") <= 1)
+    if s.startswith("-"):
+        s = s[1:]  # Remove leading minus
+    return s.replace(".", "", 1).isdigit() and s.count(".") <= 1
+
+def p_value(what, tab, where):
+    print("\n",what, end="\t", sep="")
+    for unit in tab:
+        try:
+            # print(round(len(unit)*where), len(unit))
+            print(f"{round(float(unit[round(len(unit)*where)]),6):>12}",end="\t")
+        except:
+            # print(round(len(unit)*where))
+            print(f"{round(float(unit[-1]),6):>12}",end="\t")
+
+
+
+if (len(sys.argv) > 1):
+    
+        with open(sys.argv[1], newline='') as csvfile:
+
+            spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            names = []
+            size = 0
+            tab = []
+            usable = []
+            std = []
+            for i, row in enumerate(spamreader):
+                if i == 0:
+                    size = len(row) - 1
+                    tab = [[] for _ in range(size)]
+                    names = row[1:]
+                    for i in range(len(names)):
+                        if (len(names[i]) > 12):
+                            names[i] = names[i][:9] + "..."
+                    continue
+                element = [None]*size
+                for i,elem in enumerate(row[1:]):
+                    if str(elem) == "":
+                        continue
+                    elem = elem   
+                    tab[i].append(elem)
+            print("\t    ",end="")
+            for i in range(size):
+                if (is_number(tab[i][0])):
+                    usable.append(tab[i])
+                    print(f"{names[i]:>12}",end="\t")
+            print("\ncount", end="")
+            for t in usable:
+                for i in range(len(t)):
+                    t[i] = float(t[i])
+                t.sort()
+                print(f"{len(t):>12}", end="\t")  
+            print("\nmean", end="\t")
+            for t in usable:
+                mean=0
+                for val in t:
+                    mean+= float(val)
+                mean = round(mean/len(usable[0]),6)
+                std.append(round(sqrt(abs(mean)),6))
+                print(f"{mean:>12}", end="\t")
+            print("\nstd", end="\t\t")
+            for i in range(0,len(usable)):
+                print(f"{std[i]:>12}",end="\t")
+            p_value("min", usable, 0)
+            p_value("25%", usable, 1/4)
+            p_value("50%", usable, 2/4)
+            p_value("75%", usable, 3/4)
+            p_value("max", usable, 1)
+
+
+
+        
